a8d0d9ebb4da027a9563d92994d15bd0d64bfff1
Chet Corcos <ccorcos@gmail.com>
2014-12-04 01:45:25 -0800

    bumped version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ccorcos:swipe',
   summary: 'A package for creating apps that swipe between pages',
-  version: '1.0.11',
+  version: '1.1.0',
   git: 'https://github.com/ccorcos/meteor-swipe.git'
 });
 
